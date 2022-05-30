18020f17638ebc56b9b58be5d7c73d2a07204c0c
AP <arjavprasad@gmail.com>
2022-05-30 11:46:30 -0500

    used axios to display posts

--- todoapp/src/Screens/Home.js
+++ todoapp/src/Screens/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Todos from '../Components/Todos';
 import AddTodo from '../Components/AddTodo';
+import axios from 'axios';
 
 class Home extends Component {
 	state = {
@@ -8,6 +9,7 @@ class Home extends Component {
 			{id: 1, content: 'buy some bananas'},
 			{id: 2, content: 'play mario kart'},
 		],
+		posts: [],
 	};
 	addTodo = newTodo => {
 		newTodo.id = Math.random();
@@ -24,12 +26,36 @@ class Home extends Component {
 			todos: todosCopy,
 		});
 	};
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+			console.log(res);
+			this.setState({
+				posts: res.data.slice(0, 10),
+			});
+		});
+	}
 	render() {
+		const {posts} = this.state;
+		const postList = posts.length ? (
+			posts.map(post => {
+				return (
+					<div className="post card" key={post.id}>
+						<div className="card-content">
+							<span className="card-title">{post.title}</span>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				);
+			})
+		) : (
+			<div className="center">No Posts Yet</div>
+		);
 		return (
 			<div className="todo-app container">
 				<h1 className="center blue-text">Todo's</h1>
 				<Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
 				<AddTodo addTodo={this.addTodo} />
+				{postList}
 			</div>
 		);
 	}
